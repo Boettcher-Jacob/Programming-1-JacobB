d70b43a3683cbcbc174b4a34aa811f0f5bd201e7
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2022-09-09 14:33:01 +0000

    Update prog54a+b

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+Length = int(input("Please enter the length: "))
+width = int(input("Please enter the width: "))
+
+print ("The length is", Length)
+print("The width is", width)
+
+area = Length * width
+perim = 2 + Length + 2 * width
+
+print("the area is", area)
+print("the permeter is", perim)

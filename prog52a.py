3254bb7c929f42a86ed20622a37df3415d06d764
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2022-09-13 14:36:48 +0000

    Update

--- prog52a.py
+++ prog52a.py
@@ -5,7 +5,7 @@ print ("The length is", Length)
 print("The width is", width)
 
 area = Length * width
-perim = 2 + Length + 2 * width
+perim = 2 * Length + 2 * width
 
 print("the area is", area)
 print("the permeter is", perim)

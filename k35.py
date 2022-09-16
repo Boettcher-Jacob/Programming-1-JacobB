0fd3613a85411734f12d134111989697e80e4c90
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2022-09-16 14:33:45 +0000

    Update 9/16

---
+++ k35.py
@@ -0,0 +1,126 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """Short hand move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+    """Turn Right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """Turn around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+
+  def put2(self):
+    """Put Two"""
+    put_beeper()
+    move()
+    put_beeper()
+
+  def put5(self):
+    """Put Five"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  def h(self):
+    """Letter H"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+
+  def e(self):
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.tr()
+    self.m()
+    self.m()
+    self.tr()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.put2()
+    self.m()
+    self.m()
+  def l(self):
+    self.tl()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.m()
+    self.m()
+  def o(self):
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.m()
+    self.tr()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+def main():
+    """ Karel code goes here! """
+    kt=ktools()
+    kt.h()
+    kt.e()
+    kt.l()
+    kt.l()
+    kt.o()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
---
+++ kareltemplate2.py
@@ -0,0 +1,90 @@
+from stanfordkarel import *
+
+
+class ktools:
+  def m(self):
+    """Short hand move"""
+    move()
+
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+
+  def tr(self):
+    """Turn Right"""
+    self.tl()
+    self.tl()
+    self.tl()
+
+  def ta(self):
+    """Turn around"""
+    self.tl()
+    self.tl()
+
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+
+  def put2(self):
+    """Put Two"""
+    put_beeper()
+    move()
+    put_beeper()
+
+  def put5(self):
+    """Put Five"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+
+  def h(self):
+    """Letter H w/ Beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+
+  def e(self):
+    self.tl()
+    self.put5()
+    self.tr()
+    self.put2()
+    self.tr()
+    self.m()
+    self.m()
+    self.tr()
+    """Letter E w/ Beepers"""
+    self.tl()
+    self.put
+def main():
+    """ Karel code goes here! """
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()

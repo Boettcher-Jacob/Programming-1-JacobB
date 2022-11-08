cf6ad7b6da4c5969ee434b4db8394da09e822d4b
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2022-11-08 15:14:48 +0000

    upstat

---
+++ dictionaries.py
@@ -0,0 +1,3 @@
+dctnry = {}
+dctnry2 = {1: "apples", 2: "oranges", 3: "pears"}
+dctnry3={"name": "John", "clr": "red", 0: [1,2,3]}
---
+++ prog53.py
@@ -0,0 +1,24 @@
+car1 = {
+  "miles": 286,
+  "gallons": 9
+}
+
+car2 = {
+  "miles": 416,
+  "gallons": 40
+}
+
+car3 = {
+  "miles": 361,
+  "gallons": 18
+}
+
+car4 = {
+  "miles": 286,
+  "gallons": 9
+}
+
+mycar=[input]
+carselected = car[mycar]
+gallons=carselected["gallons"]
+miles = carselected["miles"

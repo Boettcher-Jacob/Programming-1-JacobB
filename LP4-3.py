55aebddd1c05702f780429d9668c735688948cd2
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2022-11-21 15:24:12 +0000

    Update

--- LP4-3.py
+++ LP4-3.py
@@ -1,16 +1,17 @@
 def main(): 
 
   Eggs = int(input("Please Input Eggs = "))
-  Dozens = Eggs / 12
+  Dozens = Eggs/12
+  
 
-if Dozens >= 0 and Dozens < 4:
-  Price = Dozens * .50
-elif Dozens >= 4 and Dozens < 6:
-  Price = Dozens * .45
-elif Dozens >= 6 and Dozens < 11:
-  Price = Dozens * .40
-elif Dozens >= 11:
-  Price = Dozens * .35
-else:
-  Bonus = Eggs - Dozens
+  if Dozens >= 0 and Dozens < 4:
+    Price = Dozens * .50
+  elif Dozens >= 4 and Dozens < 6:
+    Price = Dozens * .45
+  elif Dozens >= 6 and Dozens < 11:
+    Price = Dozens * .40
+  elif Dozens >= 11:
+    Price = Dozens * .35
+
+  print("Price", Price)
   
